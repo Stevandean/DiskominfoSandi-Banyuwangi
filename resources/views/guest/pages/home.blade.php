2be88a3a50eeb6f9aa6dbad42ec27e831652761b
Stevandeann <stevandeanachmad1@gmail.com>
2022-08-30 10:17:47 +0700

    Styling Layanan and Home Page

--- resources/views/guest/pages/home.blade.php
+++ resources/views/guest/pages/home.blade.php
@@ -75,7 +75,7 @@
     </div>
 
 <!-- ** AWAL PELAYANAN ** -->
-<div class="p-4 px-5 pb-40 pt-20 bg-gray-100">
+    <div class="p-4 px-5 pb-40 pt-20 bg-gray-100">
       <h1 class=" py-5 text-black-600 text-4xl font-semibold text-center col-start-2">
         Layanan
       </h1>
@@ -83,21 +83,21 @@
       <div class="flex flex-wrap justify-center gap-6 md:gap-6 xl:gap-8">
 
         <!-- card -->
-        <a href="/layanan" class="relative group block p-6  max-w-sm bg-gradient-to-r from-[#ebf4f5] to-[#b5c6e0] hover:from-[#29323c] hover:to-[#485563] rounded-lg border border-gray-200 shadow-md hover:text-[#F6F6F6]">
+        <a href="/layanan" class="relative group block p-6  max-w-sm bg-gradient-to-r from-[#ebf4f5] to-[#b5c6e0]  rounded-lg border border-gray-200 shadow-md">
           <div class="border rounded-full bg-sky-500 group-hover:bg-white inline-block p-3 absolute -top-5">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="fill-white group-hover:fill-sky-500" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="fill-white group-hover:fill-sky-600" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
             </svg>
           </div>
-          <h5 class="mb-2 text-lg font-bold tracking-tight group-hover:text-[#F6F6F6]">Noteworthy technology acquisitions 2021</h5>
-          <p class="font-normal text-gray-700 text-base group-hover:text-gray-300">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          <h5 class="mb-2 text-lg font-bold tracking-tight">Noteworthy technology acquisitions 2021</h5>
+          <p class="font-normal text-gray-700 text-base">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
         </a>
         <!-- end card -->
 
         <!-- card [jangan dihapus, untuk logo] -->
-        <a href="/layanan" class="group block p-6  max-w-sm bg-gradient-to-r from-[#ebf4f5] to-[#b5c6e0] hover:from-[#29323c] hover:to-[#485563] rounded-lg border border-gray-200 shadow-md">
-          <h5 class="mb-2 text-lg font-bold tracking-tight group-hover:text-[#F6F6F6] ">Lihat Selengkapnya</h5>
-          <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="mx-auto w-12 mt-6 group-hover:fill-gray-300" viewBox="0 0 16 16">
+        <a href="/layanan" class="group block p-6  max-w-sm bg-gradient-to-r from-[#ebf4f5] to-[#b5c6e0] rounded-lg border border-gray-200 shadow-md">
+          <h5 class="mb-2 text-lg font-bold tracking-tight ">Lihat Selengkapnya</h5>
+          <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor " class="mx-auto w-12 mt-6 group-hover:fill-sky-600" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
           </svg>
         </a>
@@ -114,9 +114,9 @@
       <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
 
         <!-- card -->
-        <a href"http://sikd.banyuwangikab.go.id/" class="relative group block mx-auto p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md bg-gradient-to-r from-[#ebf4f5] to-[#b5c6e0] hover:from-[#29323c] hover:to-[#485563]">
+        <a href"http://sikd.banyuwangikab.go.id/" class="relative group block mx-auto p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md bg-gradient-to-r from-[#ebf4f5] to-[#b5c6e0]">
           <div class="flex">
-            <p class="text-center font-semibold text-lg text-gray-700 group-hover:text-[#F6F6F6] ">Sistem Informasi Keuangan Daerah.</p>
+            <p class="text-center font-semibold text-lg text-gray-700">Sistem Informasi Keuangan Daerah.</p>
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 flex-none group-hover:fill-sky-600" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
             </svg>
@@ -127,7 +127,7 @@
       </div>
     </div>
     <!-- ** AKHIR LINK TERKAIT ** -->  
-    @endsection
+  @endsection
 
 
   
--- resources/views/guest/pages/layanan.blade.php
+++ resources/views/guest/pages/layanan.blade.php
@@ -6,63 +6,23 @@
 @endpush
 
 @section('container')
-	<section class="section-galeri">
-		<div class="body-galeri">
-			<div class="container-galeri">
-				<div class="card">
-					<a href="#">
-						<div class="content">
-							<div class="imgLayanan">
-								<img src="/images/helpdesk.png" width="50px" height="50px">
-							</div>
-							<div class="contentBx">
-								<h3 class="h3-galeri">Helpdesk</h3>
-							</div>
-						</div>
-						<ul class="sci">
-							<li>
-								<a>IT Help Desk merupakan sebuah center point dimana masalah atau issue dilaporkan dan diatur secara terurut dan diorganisasikan. IT Help Desk merupakan bagian pelengkap dari sebuah fungsi pelayanan, dan bertanggung jawab sebagai sumber dari pemecahan masalah atau issue lainnya.</a>
-							</li>
-						</ul>
-					</a>
-				</div>
-				<div class="card">
-					<a href="https://pengaduan.banyuwangikab.go.id/">
-						<div class="content">
-							<div class="imgLayanan">
-								<img src="/images/pengaduan.png" width="50px" height="50px">
-							</div>
-							<div class="contentBx">
-								<h3 class="h3-galeri">Pengaduan</h3>
-							</div>
-						</div>
-						<ul class="sci">
-							<li>
-								<a>IT Help Desk merupakan sebuah center point dimana masalah atau issue dilaporkan dan diatur secara terurut dan diorganisasikan. IT Help Desk merupakan bagian pelengkap dari sebuah fungsi pelayanan, dan bertanggung jawab sebagai sumber dari pemecahan masalah atau issue lainnya.</a>
-							</li>
-						</ul>
-					</a>
-				</div>
-				<div class="card">
-					<a href="#">
-						<div class="content">
-							<div class="imgLayanan">
-								<img class="img-cctv" src="/images/cctv.png">
-							</div>
-							<div class="contentBx">
-								<h3 class="h3-galeri">CCTV</h3>
-							</div>
-						</div>
-						<ul class="sci">
-							<li>
-								<a>IT Help Desk merupakan sebuah center point dimana masalah atau issue dilaporkan dan diatur secara terurut dan diorganisasikan. IT Help Desk merupakan bagian pelengkap dari sebuah fungsi pelayanan, dan bertanggung jawab sebagai sumber dari pemecahan masalah atau issue lainnya.</a>
-							</li>
-						</ul>
-					</a>
-				</div>
+		<div class="flex flex-wrap relative justify-center gap-7 p-10 ">
+			@foreach ($services as $service)
+			<div class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md md:basis-1/3">
+				<svg class="mb-2 w-10 h-10 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd"></path><path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path></svg>
+				<a href="#">
+					<h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900">{{ $service -> name }}</h5>
+				</a>
+				<p class="mb-3 font-normal text-gray-500 ">{{ Str::limit($service -> description, 90, '...') }}</p>
+				<a href="{{ $service -> link }}" class="inline-flex items-center text-blue-600 hover:underline">
+					Lihat Selengkapnya
+					<svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+				</a>
+			</div>
+			@endforeach
+			<div class="w-full">
+				{{ $services -> links () }}
 			</div>
 		</div>
-	</section>
-	<div class="circle"></div>
 	
 @endsection
