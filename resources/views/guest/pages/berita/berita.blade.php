934167afb91b4ed4f53308b284c90addd00d40dc
Stevandeann <stevandeanachmad1@gmail.com>
2022-08-30 14:15:18 +0700

    Add  Back Top Top Button and Styling Berita Search

--- resources/views/guest/pages/berita/berita.blade.php
+++ resources/views/guest/pages/berita/berita.blade.php
@@ -3,9 +3,6 @@
 @section('container')
     <!-- Page Berita -->
     <div class="min-h-screen transition-all ease-in duration-500 ">
-        <!-- gambar untuk latar belakang -->
-        <!-- <img src="/images/gajah-oling.svg" alt="gambar gajah" srcset=""
-            class="absolute right-0 -top-2 md:-top-20 lg:-top-[200px] -z-10 opacity-50 w-3/5"> -->
         <div class="container-berita container mx-auto columns-auto">
             <p class=" mx-auto text-4xl p-8 max-w-lg text-center">
                 <span class="bg-white font-semibold px-2">
@@ -14,35 +11,15 @@
             </p>
             
             <!-- pencarian 🔍 -->
-            <div class="container pb-10 ">
-                <form action="/berita" class="flex items-center max-w-xl mx-auto">   
-                    <!-- <select id="cateory" name="category" class="block p-2.5 basis-5/12 mr-1 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:outline-blue-500 focus:border-blue-500 ">
-                        <option value="" selected>pilih kategory</option>
-                        <option value="goverment">Goverment</option>
-                        <option value="tecnology">Teknologi</option>
-                        <option value="berita">Berita</option>
-                    </select> -->
-                    
-                    <!-- <div class="relative w-full">
-                        <input type="text" name="search" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-blue-500 focus-within:border-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 "
-                            placeholder="Cari Berita Menarik">
-                            <button type="submit" class="inline">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                                <span class="sr-only">Search</span>
-                            </button>
-                    </div> -->
-                    <div class="relative mx-auto text-gray-600 w-full">
-                        <input class="border-2 border-gray-300 bg-white h-10 rounded-full px-3 text-sm focus:outline-none w-full" type="text" name="search" id="simple-search"  placeholder="Cari Berita Menarik">
-                        <button type="submit" class="absolute right-0 top-0 mt-3 mr-2">
-                            <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                                version="1.1" id="Capa_1" x="0px" y="0px"
-                                viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;"
-                                xml:space="preserve"
-                                width="512px" height="512px">
-                                <path
-                                    d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
-                            </svg>
-                        </button>
+            <div class="container pb-10 px-5">
+                <form class="px-5" action="/berita">   
+                    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+                    <div class="relative max-w-2xl mx-auto">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    </div>
+                        <input type="search" name="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-full border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Cari Berita Menarik" >
+                        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-gradient-to-r from-[#595cff] to-[#54b8ff] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2">Search</button>
                     </div>
                 </form>
             </div>
